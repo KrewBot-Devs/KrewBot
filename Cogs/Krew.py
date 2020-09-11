a5b7b736251e80378f5e05f5d641ad999ac95c53
unknown <gdemeralddragon@gmail.com>
2020-09-11 11:49:35 -0400

    added items section

--- Cogs/Krew.py
+++ Cogs/Krew.py
@@ -26,5 +26,16 @@ class Krew(commands.Cog):
         embed.add_field(name="Spain", value="Raft 1: 500\nRaft 2: 1300\nRaft 3: 2400\nTrader 1: 4350\nBoat 1: 6900\nBoat 2: 11k\nBoat 3: 16k\nDestroyer 1: 50k\nDestroyer 2: 80k\nDestroyer 3: 130k\nBaby Fancy: 8k\nRoyal Fortune: 70k\nCalm Spirit: 120k\nQueen Barb's Justice: 200k", inline=True)
         embed.add_field(name="Jamaica", value="Trader 2: 11k\nTrader 3: 30k\nBaby Fancy 2: 40k\nRoyal Fortune 2: 110k\nCalm Spirit 2: 170k\nQueen Barb's Justice 2: 350k", inline=True)
         await ctx.send(embed=embed)
+
+
+    @commands.command(name='items')
+    async def items(self, ctx):
+        embed=discord.Embed(title='Item Prices', color=discord.Color.orange())
+        embed.add_field(name='Brazil', value='Cannon Distance Upgrade: 4k\nAttack Speed Upgrade: 2k\nDamage Upgrade: 5k\nShip Speed Upgrade: 3k\nBruiser: 20k', inline=True)
+        embed.add_field(name='Guinea', value='Cannon Distance Upgrade: 4k\nAttack Speed Upgrade: 2k\nDamage Upgrade: 5k\nShip Speed Upgrade: 3k\nDrifter: 25k')
+        embed.add_field(name='Labrador', value='Steel Cannon: 35k\nCannon Distance Upgrade: 4k\nAttack Speed Upgrade: 2k\nDamage Upgrade: 5k\nShip Speed Upgrade: 3k')
+        embed.add_field(name='Spain', value='Cannon Distance Upgrade: 4k\nAttack Speed Upgrade: 2k\nDamage Upgrade: 5k\nShip Speed Upgrade: 3k')
+        embed.add_field(name='Jamaica', value='Air Pegleg: 22k\nBlue Gunpowder: 50k\nCannon Distance Upgrade: 4k\nAttack Speed Upgrade: 2k\nDamage Upgrade: 5k\nShip Speed Upgrade: 3k\nDemolisher: 100k')
+        await ctx.send(embed=embed)
 def setup(bot):
     bot.add_cog(Krew(bot))
