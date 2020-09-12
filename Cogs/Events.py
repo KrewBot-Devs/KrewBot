3d37bc17cb3e80063334ec7ec618ca0207ac7763
Shamus Schneider <67710582+Sh-wayz@users.noreply.github.com>
2020-09-12 18:56:36 -0400

    Update Events.py

--- Cogs/Events.py
+++ Cogs/Events.py
@@ -10,7 +10,7 @@ class Events(commands.Cog):
 
     @commands.Cog.listener()
     async def on_ready(self):
-        activity = discord.Game(name='Krew.io with emerald', type=3)
+        activity = discord.Game(name='Krew.io | https://github.com/KrewBot-Devs/KrewBot', type=3)
         await self.bot.change_presence(status=discord.Status.online, activity=activity)
 
 
