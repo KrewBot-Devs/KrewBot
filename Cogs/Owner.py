2833dfba0c4d8a865b909283a76b6e2ba7c7d093
Shamus Schneider <67710582+Sh-wayz@users.noreply.github.com>
2020-09-28 20:43:18 -0400

    added ownerhelp YEET

--- Cogs/Owner.py
+++ Cogs/Owner.py
@@ -32,6 +32,72 @@ class Owner(commands.Cog):
             await ctx.send(embed=discord.Embed(color=discord.Color.orange, description=f'```{log_file.read()}```'))
 
         os.remove('git_pull_log')
+   @commands.group(name='ownerhelp')
+   @commands.is_owner()
+    async def ownerhelp(self, ctx):
+        if ctx.invoked_subcommand is None:
+            async with ctx.typing():
+                await asyncio.sleep(1)
+                help_embed = discord.Embed(color=discord.Color.orange())
+                help_embed.set_author(
+                    name="KrewBot Owner cmds",
+                    icon_url="https://lh3.googleusercontent.com/proxy/IE0HkL7sE5XsN81L0GNI8wakwUKCACIgRhLxQQcICPUNrc5rArnvjKO0BfweqzqM9tHpPYTaHhWRkSSpKFZO6NxB3AXTmnKOTlDEUtseNR2PaZQnmp4W7w")
+
+                help_embed.add_field(name="Cogs", value=f"``{ctx.prefix}ownerhelp cogs``", inline=True)
+                help_embed.add_field(name='Git', value=f"``{ctx.prefix}ownerhelp git``", inline=True)
+                help_embed.add_field(name='Other, value=f"``{ctx.prefix}ownerhelp other", inline=True)
+            help_embed.set_footer(text="Made by the only people who can use this command.")
+            await ctx.send(embed=help_embed)
+
+
+    @ownerhelp.command(name='cogs')
+    async def help_admin(self, ctx):
+
+        async with ctx.typing():
+            await asyncio.sleep(1)
+
+            help_embed = discord.Embed(color=discord.Color.orange())
+            help_embed.set_author(
+            name='Cog cmds',
+            icon_url="https://lh3.googleusercontent.com/proxy/IE0HkL7sE5XsN81L0GNI8wakwUKCACIgRhLxQQcICPUNrc5rArnvjKO0BfweqzqM9tHpPYTaHhWRkSSpKFZO6NxB3AXTmnKOTlDEUtseNR2PaZQnmp4W7w")
+
+            help_embed.add_field(
+            name='**Cooooooogs**',
+            value=f'**{ctx.prefix}load** *\{cogname\}* - loads cog lol what did you think.\n'
+            f'**{ctx.prefix}unload** *\{cog name\}* - seems pretty obvious to me tbh.\n'
+            f'**{ctx.prefix}reload** *Number of messages.* - Remove set amount of messages.\n'
+                , inline=False)
+
+            help_embed.set_footer(text="Need more help? You're an owner go fix it.)
+
+            await ctx.send(embed=help_embed)
+
+
+    @ownerhelp.command(name='git')
+    async def help_admin(self, ctx):
+
+        async with ctx.typing():
+            await asyncio.sleep(1)
+
+            help_embed = discord.Embed(color=discord.Color.orange())
+            help_embed.set_author(
+            name='git cmds',
+            icon_url="https://lh3.googleusercontent.com/proxy/IE0HkL7sE5XsN81L0GNI8wakwUKCACIgRhLxQQcICPUNrc5rArnvjKO0BfweqzqM9tHpPYTaHhWRkSSpKFZO6NxB3AXTmnKOTlDEUtseNR2PaZQnmp4W7w")
+
+            help_embed.add_field(
+            name='**git is the best**',
+            value=f'**{ctx.prefix}gitpull** *\{cogname\}* - <:megathonk:683696698459029566> whaddaya think.\n'
+                , inline=False)
+
+            help_embed.set_footer(text="Need more help? You're an owner go fix it.)
+
+      
+    @ownerhelp.command(name='git')
+    async def help_admin(self, ctx):
+
+        async with ctx.typing():
+            await asyncio.sleep(1)    
+            await ctx.send('https://i.kym-cdn.com/entries/icons/original/000/027/081/wow.jpg)
 
 
 def setup(bot):
